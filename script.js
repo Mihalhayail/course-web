f9b6500a2957061d7c2c73f9cc484cd62bda28af
Mihalhayail <mihalhayail85@gmail.com>
2025-09-01 03:43:30 +0700

    add auth button on header

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const menuToggle = document.querySelector(".header-toggle");
 const nav = document.querySelector("nav ul");
+const auth = document.querySelector(".header-auth");
 const spans = document.querySelectorAll(".header-toggle span");
 
 menuToggle.addEventListener("click", function () {
   nav.classList.toggle("slide");
+  auth.classList.toggle("slide");
   spans.forEach((span) => span.classList.toggle("slide"));
 });
