1a9b275aa57f071269c267a77e09081fa3a07314
Mihalhayail <mihalhayail85@gmail.com>
2025-09-01 06:19:00 +0700

    add buttom container and fix other components

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ menuToggle.addEventListener("click", function () {
   auth.classList.toggle("slide");
   spans.forEach((span) => span.classList.toggle("slide"));
 });
+
+// Header hide on scroll
+let lastScrollTop = 0;
+const header = document.querySelector(".header");
+window.addEventListener("scroll", function () {
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop > lastScrollTop) {
+    header.classList.add("hide");
+  } else {
+    header.classList.remove("hide");
+  }
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+});
